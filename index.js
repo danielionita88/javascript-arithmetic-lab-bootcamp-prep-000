2b16576fdf069fb2d9cf8430621261eeefea55c6
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-07-18 16:37:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ function add(a,b) {return a+b}
 function subtract(a,b) {return a-b}
 function multiply(a,b) {return a*b}
 function divide(a,b) {return a/b}
-function inc(n) {return n++}
+function inc(n) {return Number +=n}
